ade51addbe442c4b055866307accfc3ac5eacd38
JEONG-BD <dev.byeongdu@gmail.com>
2025-12-17 11:57:22 +0900

    Test module

---
+++ TestSupport/src/main/java/com/example/demo/StandardOutputTest.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+
+public class StandardOutputTest {
+
+    private PrintStream _savedOut;
+    private ByteArrayOutputStream _actual;
+
+    @BeforeEach
+    public void setUp() {
+        _savedOut = System.out;
+        _actual = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(new BufferedOutputStream(_actual)));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(_savedOut);
+    }
+
+    protected String getActualOutput() {
+        System.out.flush();
+        return _actual.toString();
+    }
+
+    protected String getExpectedOutput(String... strs) {
+        ByteArrayOutputStream expected = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(new BufferedOutputStream(expected));
+        for (String s : strs) {
+            out.println(s);
+        }
+        out.flush();
+        return expected.toString();
+    }
+}
+
